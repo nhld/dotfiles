80cc54f8607780ac3cdbb2eb84b71ddbc984f072
nhxnl <nahnera@gmail.com>
2024-01-24 15:23:09 +0700

    feat(nvim): refactor options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,62 +1,65 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+local g = vim.g
+local opt = vim.opt
 
-vim.g.root_spec = { "lsp", { ".git", "lua", }, "cwd" }
+g.root_spec = { "lsp", { ".git", "lua", }, "cwd" }
 
-vim.wo.nu = true
-vim.wo.rnu = true
-vim.o.cursorline = true
-vim.o.winblend = 0
-vim.o.wildoptions = 'pum'
-vim.o.pumblend = 0
-vim.o.background = 'dark'
-vim.o.autoindent = true
-vim.o.smartindent = true
-vim.o.wrap = false
-vim.o.showcmd = true
-vim.o.cmdheight = 1
-vim.o.incsearch = true
-vim.o.scrolloff = 10
-vim.o.laststatus = 3
+opt.showmode = true
+opt.nu = true
+opt.rnu = true
+opt.conceallevel = 0
+opt.cursorline = true
+opt.winblend = 0
+opt.wildmenu = true
+opt.wildoptions = 'pum'
+opt.pumblend = 10
+opt.background = 'dark'
+opt.autoindent = true
+opt.smartindent = true
+opt.wrap = false
+opt.showcmd = true
+opt.cmdheight = 1
+opt.incsearch = true
+opt.scrolloff = 10
+opt.laststatus = 3
 --vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = 'yellow' })
 
 -- Tab
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
-vim.o.expandtab = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.expandtab = true
 
 -- Set highlight on search
-vim.o.hlsearch = true
+opt.hlsearch = true
 
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+opt.mouse = 'a'
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+opt.clipboard = 'unnamedplus'
 
 -- Enable break indent
-vim.o.breakindent = true
+opt.breakindent = true
 
 -- Save undo history
-vim.o.undofile = true
+opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+opt.signcolumn = 'yes'
 
 -- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
+opt.updatetime = 250
+opt.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect,noinsert,menu'
+opt.completeopt = 'menuone,noselect,noinsert,menu'
 
 -- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
+opt.termguicolors = true
