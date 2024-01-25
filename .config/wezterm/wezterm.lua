2c31bcf0418da7ab31e922c856a733a2b1e95d6e
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-01-25 08:12:45 +0700

    feat(wezterm): add configs

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,29 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.colors = {}
+config.colors.background = '#111111'
+
+config.scrollback_lines = 2000
+config.font_size = 12
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+config.colors.tab_bar = {
+  background = '#1f2329',
+}
+
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+
+return config
