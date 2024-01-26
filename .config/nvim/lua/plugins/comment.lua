7a98583d1697266ad131810be1c170fa70320612
nhxnl <nahnera@gmail.com>
2024-01-26 13:19:58 +0700

    refactor(nvim): reduce startup time

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,6 +1,7 @@
 return {
   'numToStr/Comment.nvim',
-  lazy = false,
+  --lazy = false,
   --event = "BufReadPre",
+  event = 'VeryLazy',
   opts = {},
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  --event = 'VeryLazy',
+  --lazy = false,
+  event = { 'BufReadPost', 'BufNewFile' },
+  --event = "VeryLazy",
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
