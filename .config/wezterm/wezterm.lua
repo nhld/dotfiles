5dce6b5299fcdfd6515fa2516d4df336849d1b5b
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-01-29 13:24:26 +0700

    feat(wezterm): change font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,7 +10,10 @@ config.colors = {}
 config.colors.background = '#111111'
 
 config.scrollback_lines = 2000
+
 config.font_size = 12
+config.font = wezterm.font 'FiraMono Nerd Font'
+
 config.window_padding = {
   left = 0,
   right = 0,
@@ -25,5 +28,7 @@ config.colors.tab_bar = {
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
+
+--config.check_for_updates = false
 
 return config
