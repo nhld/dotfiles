250e717088f2d2db30615dd3e5458584fc609cf1
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-01-29 13:27:12 +0700

    chore(nvim): clean up

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -79,20 +79,6 @@ opt.splitright = true
 opt.splitbelow = true
 --opt.smoothscroll = true
 
-vim.api.nvim_create_autocmd({
-  "WinResized", -- or WinResized on NVIM-v0.9 and higher
-  "BufWinEnter",
-  "CursorHold",
-  "InsertLeave",
-
-  -- include this if you have set `show_modified` to `true`
-  --"BufModifiedSet",
-}, {
-  group = vim.api.nvim_create_augroup("barbecue.updater", {}),
-  callback = function()
-    require("barbecue.ui").update()
-  end,
-})
 -- Highlight on yank
 -- :help vim.highlight.on_yank()
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
