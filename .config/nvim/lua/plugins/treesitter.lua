c0a1e393ea426d4a5cfc569309257f21eabbbd10
nhxnl <nahnera@gmail.com>
2024-02-02 20:07:40 +0700

    fix(nvim): use treesitter for markdown K hover

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ local config = function()
   vim.defer_fn(function()
     require('nvim-treesitter.configs').setup {
       -- Add languages to be installed here that you want installed for treesitter
-      ensure_installed = { 'cpp', 'go', 'lua', 'python', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'html', 'json', 'css', 'markdown' },
+      ensure_installed = { 'cpp', 'go', 'lua', 'python', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'html', 'json', 'css', 'markdown', 'markdown_inline', 'regex' },
 
       -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
       auto_install = true,
