20da9783122ba46e5c2919ceff0cbc4957b20594
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-02-05 15:31:09 +0700

    chore(nvim): clean up

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -118,20 +118,14 @@ local config = function()
 end
 
 return {
-  -- Autocompletion
   'hrsh7th/nvim-cmp',
   config = config,
   event = { "InsertEnter", "CmdlineEnter" },
   dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
-
-    -- Adds LSP completion capabilities
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
-
-    -- Adds a number of user-friendly snippets
     'rafamadriz/friendly-snippets',
     'hrsh7th/cmp-cmdline'
   },
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,8 @@
 local config = function()
   vim.defer_fn(function()
     require('nvim-treesitter.configs').setup {
-      -- Add languages to be installed here that you want installed for treesitter
       ensure_installed = { 'cpp', 'go', 'lua', 'python', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'html', 'json', 'css', 'markdown', 'markdown_inline' },
-
-      -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
       auto_install = true,
-
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
