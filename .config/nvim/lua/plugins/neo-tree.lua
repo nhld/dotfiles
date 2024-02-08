8503ac1cf3e609264dc33168af92c79adf3a1cfd
nhxnl <nahnera@gmail.com>
2024-02-08 15:19:24 +0700

    feat(nvim): change git signs on lualine and signcolumn

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -6,4 +6,32 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
   },
+  config = function()
+    require('neo-tree').setup({
+      default_component_config = {
+        window = {
+          mappings = {
+            ["h"] = function(state)
+              local node = state.tree:get_node()
+              if node.type == 'directory' and node:is_expanded() then
+                require 'neo-tree.sources.filesystem'.toggle_directory(state, node)
+              else
+                require 'neo-tree.ui.renderer'.focus_node(state, node:get_parent_id())
+              end
+            end,
+            ["l"] = function(state)
+              local node = state.tree:get_node()
+              if node.type == 'directory' then
+                if not node:is_expanded() then
+                  require 'neo-tree.sources.filesystem'.toggle_directory(state, node)
+                elseif node:has_children() then
+                  require 'neo-tree.ui.renderer'.focus_node(state, node:get_child_ids()[1])
+                end
+              end
+            end,
+          }
+        }
+      }
+    })
+  end
 }
