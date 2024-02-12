18cc6d11a138f427e16817c1d50acb801b70210a
nhxnl <nahnera@gmail.com>
2024-02-13 00:20:55 +0700

    chore(nvim): clean up

--- .config/nvim/lua/plugins/ibl.lua
+++ .config/nvim/lua/plugins/ibl.lua
@@ -44,14 +44,11 @@ local opts = {
 local config = function()
 	local hooks = require("ibl.hooks")
 	hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-	--hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
-	--hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
 	require("ibl").setup(opts)
 end
 
 return {
 	"lukas-reineke/indent-blankline.nvim",
-	--enabled = false,
 	config = config,
 	event = "VeryLazy",
 	main = "ibl",
