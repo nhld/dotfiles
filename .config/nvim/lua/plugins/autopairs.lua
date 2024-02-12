bbae59eb7f8a286b8af44da1ff3a5c0429c81f41
nhxnl <nahnera@gmail.com>
2024-02-12 20:57:33 +0700

    chore(nvim): clean up

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,19 +1,16 @@
 local config = function()
-  require("nvim-autopairs").setup {}
-  local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-  local cmp = require('cmp')
-  cmp.event:on(
-    'confirm_done',
-    cmp_autopairs.on_confirm_done()
-  )
+	require("nvim-autopairs").setup({})
+	local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+	local cmp = require("cmp")
+	cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 end
 
 return {
-  "windwp/nvim-autopairs",
-  event = "InsertEnter",
-  dependencies = { 'hrsh7th/nvim-cmp' },
-  opts = {
-    disable_filetype = { 'TelescopePrompt' },
-  },
-  config = config,
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	dependencies = { "hrsh7th/nvim-cmp" },
+	opts = {
+		disable_filetype = { "TelescopePrompt" },
+	},
+	config = config,
 }
