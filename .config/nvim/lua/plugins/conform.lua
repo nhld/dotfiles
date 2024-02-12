17826e21fbfd90def4a601dbba6087ed448feee9
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-02-12 20:59:21 +0700

    fix(nvim): fix navic on attach, show formatter on lualine

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,79 +1,42 @@
--- Formatting.
 return {
-	{
-		"stevearc/conform.nvim",
-		event = {
-			--'LspAttach',
-			"BufWritePre",
+	"stevearc/conform.nvim",
+	event = {
+		--'LspAttach',
+		"BufWritePre",
+	},
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
 		},
-		cmd = { "ConformInfo" },
-		keys = {
-			{
-				"<leader>f",
-				function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end,
-				mode = "",
-				desc = "Format buffer",
-			},
+	},
+	opts = {
+		formatters_by_ft = {
+			javascript = { "prettierd" },
+			javascriptreact = { "prettierd" },
+			typescript = { "prettierd" },
+			typescriptreact = { "prettierd" },
+			vue = { "prettierd" },
+			css = { "prettierd" },
+			scss = { "prettierd" },
+			less = { "prettierd" },
+			html = { "prettierd" },
+			jsonc = { "prettierd" },
+			yaml = { "prettierd" },
+			markdown = { "prettierd" },
+			["markdown.mdx"] = { "prettierd" },
+			json = { "prettierd" },
+			lua = { "stylua" },
+			--python = { "isort" },
 		},
-		opts = {
-			formatters_by_ft = {
-				javascript = { "prettierd" },
-				javascriptreact = { "prettierd" },
-				typescript = { "prettierd" },
-				typescriptreact = { "prettierd" },
-				css = { "prettierd" },
-				scss = { "prettierd" },
-				less = { "prettierd" },
-				html = { "prettierd" },
-				json = { "prettierd" },
-				lua = { "stylua" },
-				python = { "isort" },
-			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-			-- format_on_save = function(bufnr)
-			--   if vim.b[bufnr].format_on_save then
-			--     return {
-			--       timeout_ms = 500,
-			--       -- Filetypes to use LSP formatting for.
-			--       lsp_fallback = vim.tbl_contains({ 'c', 'json', 'jsonc', }, vim.bo[bufnr].filetype),
-			--     }
-			--   end
-			-- end,
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
 		},
-		--init = function()
-		-- Use conform for gq.
-		-- vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-		--
-		-- -- Configure format on save inside my dotfiles and personal projects.
-		-- vim.api.nvim_create_autocmd('BufEnter', {
-		--     desc = 'Configure format on save',
-		--     callback = function(args)
-		--         local path = vim.api.nvim_buf_get_name(args.buf)
-		--         path = vim.fs.normalize(path)
-		--         vim.b[args.buf].format_on_save = vim.iter({ vim.env.XDG_CONFIG_HOME, vim.g.personal_projects_dir })
-		--             :any(function(folder)
-		--                 return vim.startswith(path, vim.fs.normalize(folder))
-		--             end)
-		--     end,
-		-- })
-		-- vim.api.nvim_create_autocmd("BufWritePre", {
-		--   pattern = "*",
-		--   callback = function(args)
-		--     require("conform").format({ bufnr = args.buf })
-		--   end,
-		-- })
-		-- require("conform").setup({
-		--   format_on_save = {
-		--     -- These options will be passed to conform.format()
-		--     timeout_ms = 500,
-		--     lsp_fallback = true,
-		--   },
-		-- })
-		--end,
 	},
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,4 @@
-local on_attach = function(_, bufnr)
+local on_attach = function(client, bufnr)
 	local nmap = function(keys, func, desc)
 		if desc then
 			desc = "LSP: " .. desc
@@ -44,6 +44,12 @@ local on_attach = function(_, bufnr)
 	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
 		vim.lsp.buf.format()
 	end, { desc = "Format current buffer with LSP" })
+
+	-- Navic stuff attach, if remove this, remove client param of on_attach func
+	local navic = require("nvim-navic")
+	if client.server_capabilities.documentSymbolProvider then
+		navic.attach(client, bufnr)
+	end
 end
 
 local function show_only_one_sign_in_sign_column() -- func called at the end of config function
@@ -123,73 +129,12 @@ end
 --show_only_one_sign_in_sign_column()
 
 local config = function()
-	--  Use :KickstartFormatToggle to toggle autoformatting on or off
-	-- local format_is_enabled = true
-	-- vim.api.nvim_create_user_command('KickstartFormatToggle', function()
-	--   format_is_enabled = not format_is_enabled
-	--   print('Setting autoformatting to: ' .. tostring(format_is_enabled))
-	-- end, {})
-	--
-	-- -- Create an augroup that is used for managing our formatting autocmds.
-	-- --      We need one augroup per client to make sure that multiple clients
-	-- --      can attach to the same buffer without interfering with each other.
-	-- local _augroups = {}
-	-- local get_augroup = function(client)
-	--   if not _augroups[client.id] then
-	--     local group_name = 'kickstart-lsp-format-' .. client.name
-	--     local id = vim.api.nvim_create_augroup(group_name, { clear = true })
-	--     _augroups[client.id] = id
-	--   end
-	--
-	--   return _augroups[client.id]
-	-- end
-	--
-	-- -- See `:help LspAttach` for more information about this autocmd event.
-	-- vim.api.nvim_create_autocmd('LspAttach', {
-	--   group = vim.api.nvim_create_augroup('kickstart-lsp-attach-format', { clear = true }),
-	--   -- This is where we attach the autoformatting for reasonable clients
-	--   callback = function(args)
-	--     local client_id = args.data.client_id
-	--     local client = vim.lsp.get_client_by_id(client_id)
-	--     local bufnr = args.buf
-	--
-	--     -- Only attach to clients that support document formatting
-	--     if not client.server_capabilities.documentFormattingProvider then
-	--       return
-	--     end
-	--
-	--     -- Tsserver usually works poorly. Sorry you work with bad languages
-	--     -- You can remove this line if you know what you're doing :)
-	--     if client.name == 'tsserver' then
-	--       return
-	--     end
-	--
-	--     -- Create an autocmd that will run *before* we save the buffer.
-	--     --  Run the formatting command for the LSP that has just attached.
-	--     vim.api.nvim_create_autocmd('BufWritePre', {
-	--       group = get_augroup(client),
-	--       buffer = bufnr,
-	--       callback = function()
-	--         if not format_is_enabled then
-	--           return
-	--         end
-	--
-	--         vim.lsp.buf.format {
-	--           async = false,
-	--           filter = function(c)
-	--             return c.id == client.id
-	--           end,
-	--         }
-	--       end,
-	--     })
-	--   end,
-	-- })
-
 	require("neodev").setup()
 	require("mason").setup()
-	--require('mason-lspconfig').setup({})
 	local mason_lspconfig = require("mason-lspconfig")
+
 	mason_lspconfig.setup({})
+
 	local servers = {
 		-- clangd = {},
 		-- gopls = {},
@@ -197,7 +142,6 @@ local config = function()
 		-- rust_analyzer = {},
 		-- tsserver = {},
 		-- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
 		lua_ls = {
 			Lua = {
 				workspace = { checkThirdParty = false },
@@ -365,7 +309,6 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
-		-- Automatically install LSPs to stdpath for neovim
 		{ "williamboman/mason.nvim", config = true },
 		{ "williamboman/mason-lspconfig.nvim" },
 		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
