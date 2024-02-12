a53eb78782a1e6d4c007271c7d2045785b805db1
nhxnl <nahnera@gmail.com>
2024-02-12 12:31:24 +0700

    feat(nvim): add keymaps and spell check config

--- .config/nvim/lua/config/colors.lua
+++ .config/nvim/lua/config/colors.lua
@@ -1,24 +1,19 @@
 -- highlight currentline number yellow
-vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = 'yellow' })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "yellow" })
 --background for popup menu (fix this pls)
 vim.api.nvim_set_hl(0, "CmpNormal", { bg = "#1f2329" })
 -- vscode like color for cmp popup menu both in editor and command
--- gray
-vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { bg = 'NONE', strikethrough = true, fg = '#808080' })
--- blue
-vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { bg = 'NONE', fg = '#569CD6' })
-vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { link = 'CmpIntemAbbrMatch' })
--- light blue
-vim.api.nvim_set_hl(0, 'CmpItemKindVariable', { bg = 'NONE', fg = '#9CDCFE' })
-vim.api.nvim_set_hl(0, 'CmpItemKindInterface', { link = 'CmpItemKindVariable' })
-vim.api.nvim_set_hl(0, 'CmpItemKindText', { link = 'CmpItemKindVariable' })
--- pink
-vim.api.nvim_set_hl(0, 'CmpItemKindFunction', { bg = 'NONE', fg = '#C586C0' })
-vim.api.nvim_set_hl(0, 'CmpItemKindMethod', { link = 'CmpItemKindFunction' })
--- front
-vim.api.nvim_set_hl(0, 'CmpItemKindKeyword', { bg = 'NONE', fg = '#D4D4D4' })
-vim.api.nvim_set_hl(0, 'CmpItemKindProperty', { link = 'CmpItemKindKeyword' })
-vim.api.nvim_set_hl(0, 'CmpItemKindUnit', { link = 'CmpItemKindKeyword' })
+vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { bg = "NONE", strikethrough = true, fg = "#808080" })
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { bg = "NONE", fg = "#569CD6" })
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { link = "CmpIntemAbbrMatch" })
+vim.api.nvim_set_hl(0, "CmpItemKindVariable", { bg = "NONE", fg = "#9CDCFE" })
+vim.api.nvim_set_hl(0, "CmpItemKindInterface", { link = "CmpItemKindVariable" })
+vim.api.nvim_set_hl(0, "CmpItemKindText", { link = "CmpItemKindVariable" })
+vim.api.nvim_set_hl(0, "CmpItemKindFunction", { bg = "NONE", fg = "#C586C0" })
+vim.api.nvim_set_hl(0, "CmpItemKindMethod", { link = "CmpItemKindFunction" })
+vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { bg = "NONE", fg = "#D4D4D4" })
+vim.api.nvim_set_hl(0, "CmpItemKindProperty", { link = "CmpItemKindKeyword" })
+vim.api.nvim_set_hl(0, "CmpItemKindUnit", { link = "CmpItemKindKeyword" })
 
 -- use yanky plugin so disabled this
 -- Highlight on yank
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,27 +1,27 @@
-local g = vim.g
 local opt = vim.opt
 
-g.root_spec = { "lsp", { ".git", "lua", }, "cwd" }
-
 opt.showmode = true
+-- Show line number and relative number
 opt.nu = true
 opt.rnu = true
 
 opt.conceallevel = 0
+-- Highlight current line that cursor's on
 opt.cursorline = true
 
 opt.winblend = 0
 
 opt.wildmenu = true
 opt.wildmode = "longest:full"
-opt.wildoptions = 'pum'
+opt.wildoptions = "pum"
 opt.pumblend = 0
 --opt.background = 'dark'
 
 opt.incsearch = true
 
+-- disable vim match paren jump
 opt.showmatch = false
-opt.matchpairs = { '(:)', '{:}', '[:]', '<:>' }
+opt.matchpairs = { "(:)", "{:}", "[:]", "<:>" }
 
 opt.autoindent = true
 opt.smartindent = true
@@ -30,7 +30,9 @@ opt.linebreak = true
 opt.wrap = false
 opt.showcmd = true
 opt.cmdheight = 1
+-- Vertical offset when moving up down
 opt.scrolloff = 10
+-- Global status line
 opt.laststatus = 3
 -- Tab
 opt.tabstop = 2
@@ -40,9 +42,9 @@ opt.expandtab = true
 -- Set highlight on search
 opt.hlsearch = true
 -- Enable mouse mode
-opt.mouse = 'a'
+opt.mouse = "a"
 -- Sync clipboard between OS and Neovim.
-opt.clipboard = 'unnamedplus'
+opt.clipboard = "unnamedplus"
 -- Enable break indent
 opt.breakindent = true
 -- Save undo history
@@ -51,13 +53,18 @@ opt.undofile = true
 opt.ignorecase = true
 opt.smartcase = true
 -- Keep signcolumn on by default
-opt.signcolumn = 'yes:3'
+opt.signcolumn = "yes:3"
 -- Decrease update time
 opt.updatetime = 200
 opt.timeoutlen = 300
 -- Set completeopt to have a better completion experience
-opt.completeopt = 'menuone,noselect,noinsert'
--- NOTE: You should make sure your terminal supports this
+opt.completeopt = "menuone,noselect,noinsert"
+-- True colors
 opt.termguicolors = true
+-- Split screens
 opt.splitright = true
 opt.splitbelow = true
+-- Spell check
+opt.spelllang = "en_us"
+opt.spell = true
+opt.spelloptions = "camel"
