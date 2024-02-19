03224191da5f44753a1d6229c8e5b94943a9b323
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-02-19 09:33:27 +0700

    feat(wezterm): config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,38 +1,29 @@
 local wezterm = require("wezterm")
-
 local config = {}
-
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
-
+config.term = "wezterm"
+config.front_end = "WebGPU"
 config.colors = {}
 config.colors.background = "#111111"
-
 config.scrollback_lines = 2000
-
 config.font_size = 12
 config.font = wezterm.font("FiraMono Nerd Font")
-
 config.window_padding = {
 	left = 0,
 	right = 0,
 	top = 0,
 	bottom = 0,
 }
-
 config.colors.tab_bar = {
 	background = "#1f2329",
 }
-
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
-
--- underline
 config.underline_position = -6
 config.underline_thickness = "250%"
 config.native_macos_fullscreen_mode = true
---config.check_for_updates = false
 
 return config
