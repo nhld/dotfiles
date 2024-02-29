9d1514bff6441dceaceb5e9716725e087d214be5
nhxnl <nahnera@gmail.com>
2024-02-29 14:06:51 +0700

    feat(nvim): config plugins keymaps

--- .config/nvim/lua/plugins/yanky.lua
+++ .config/nvim/lua/plugins/yanky.lua
@@ -5,22 +5,12 @@ local opts = {
 }
 
 local keys = {
-	{
-		"p",
-		"<Plug>(YankyPutAfter)",
-		mode = { "n", "x" },
-		desc = "Put yanked text after cursor",
-	},
-	{
-		"P",
-		"<Plug>(YankyPutBefore)",
-		mode = { "n", "x" },
-		desc = "Put yanked text before cursor",
-	},
+	{ "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
+	{ "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
 	{ "=p", "<Plug>(YankyPutAfterLinewise)", desc = "Put yanked text in line below" },
 	{ "=P", "<Plug>(YankyPutBeforeLinewise)", desc = "Put yanked text in line above" },
-	--{ "[y", "<Plug>(YankyCycleForward)", desc = "Cycle forward through yank history" },
-	--{ "]y", "<Plug>(YankyCycleBackward)", desc = "Cycle backward through yank history" },
+	{ "[y", "<Plug>(YankyCycleForward)", desc = "Cycle forward through yank history" },
+	{ "]y", "<Plug>(YankyCycleBackward)", desc = "Cycle backward through yank history" },
 	{ "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yanky Yank" },
 }
 
