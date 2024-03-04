d15d216caf9d7d1c6304940d325745f7ebd5d014
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-03-04 14:48:42 +0700

    feat(nvim): remove neodev plugin

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,58 +1,44 @@
 local keys = {
-	{
-		"<leader>f",
-		function()
-			require("conform").format({ async = true, lsp_fallback = true })
-		end,
-		mode = "",
-		desc = "Format buffer",
-	},
+  {
+    "<leader>f",
+    function()
+      require("conform").format { async = true, lsp_fallback = true }
+    end,
+    mode = "",
+    desc = "Format buffer",
+  },
 }
 
 local opts = {
-	formatters = {
-		prettier = {
-			---@diagnostic disable-next-line: unused-local
-			args = function(self, ctx) -- gotta pass self as the first arg, see :help Conform-self-args
-				if vim.endswith(ctx.filename, ".ejs") then
-					return { "--stdin-filepath", "$FILENAME", "--parser", "html" }
-				end
-				return { "--stdin-filepath", "$FILENAME" }
-			end,
-		},
-	},
-
-	formatters_by_ft = {
-		javascript = { "prettierd", "prettier" },
-		javascriptreact = { "prettierd", "prettier" },
-		typescript = { "prettierd", "prettier" },
-		typescriptreact = { "prettierd", "prettier" },
-		vue = { "prettierd", "prettier" },
-		css = { "prettierd", "prettier" },
-		scss = { "prettierd", "prettier" },
-		less = { "prettierd", "prettier" },
-		html = { "prettier" }, -- FIX: this is sketchy
-		jsonc = { "prettierd", "prettier" },
-		yaml = { "prettierd", "prettier" },
-		markdown = { "prettierd", "prettier" },
-		["markdown.mdx"] = { "prettierd", "prettier" },
-		json = { "prettierd", "prettier" },
-		lua = { "stylua" },
-		go = { "gofumpt", "goimports-reviser" },
-	},
-
-	format_on_save = {
-		timeout_ms = 500,
-		lsp_fallback = true,
-	},
-
-	notify_on_error = true,
+  formatters_by_ft = {
+    javascript = { "prettierd", "prettier" },
+    javascriptreact = { "prettierd", "prettier" },
+    typescript = { "prettierd", "prettier" },
+    typescriptreact = { "prettierd", "prettier" },
+    vue = { "prettierd", "prettier" },
+    css = { "prettierd", "prettier" },
+    scss = { "prettierd", "prettier" },
+    less = { "prettierd", "prettier" },
+    html = { "prettierd", "prettier" }, --NOTE: Not working with .ejs
+    jsonc = { "prettierd", "prettier" },
+    yaml = { "prettierd", "prettier" },
+    markdown = { "prettierd", "prettier" },
+    ["markdown.mdx"] = { "prettierd", "prettier" },
+    json = { "prettierd", "prettier" },
+    lua = { "stylua" },
+    go = { "gofumpt", "goimports-reviser" },
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+  notify_on_error = true,
 }
 
 return {
-	"stevearc/conform.nvim",
-	event = "VeryLazy",
-	cmd = { "ConformInfo" },
-	keys = keys,
-	opts = opts,
+  "stevearc/conform.nvim",
+  event = "VeryLazy",
+  cmd = { "ConformInfo" },
+  keys = keys,
+  opts = opts,
 }
