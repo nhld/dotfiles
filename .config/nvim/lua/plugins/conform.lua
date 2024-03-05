daa67d5dc3a6f61c993f78e91c4fb91cdaf8b30e
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-03-05 14:32:41 +0700

    feat(nvim): config conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -9,36 +9,49 @@ local keys = {
   },
 }
 
-local opts = {
-  formatters_by_ft = {
-    javascript = { "prettierd", "prettier" },
-    javascriptreact = { "prettierd", "prettier" },
-    typescript = { "prettierd", "prettier" },
-    typescriptreact = { "prettierd", "prettier" },
-    vue = { "prettierd", "prettier" },
-    css = { "prettierd", "prettier" },
-    scss = { "prettierd", "prettier" },
-    less = { "prettierd", "prettier" },
-    html = { "prettierd", "prettier" }, --NOTE: Not working with .ejs
-    jsonc = { "prettierd", "prettier" },
-    yaml = { "prettierd", "prettier" },
-    markdown = { "prettierd", "prettier" },
-    ["markdown.mdx"] = { "prettierd", "prettier" },
-    json = { "prettierd", "prettier" },
-    lua = { "stylua" },
-    go = { "gofumpt", "goimports-reviser" },
-  },
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
-  notify_on_error = true,
-}
+local config = function()
+  require("conform").setup {
+    formatters = {
+      prettier = { -- see https://github.com/stevearc/conform.nvim/issues/208
+        ---@diagnostic disable-next-line: unused-local
+        args = function(self, ctx)
+          if vim.endswith(ctx.filename, ".ejs") then
+            return { "--stdin-filepath", "$FILENAME", "--parser", "html" }
+          end
+          return { "--stdin-filepath", "$FILENAME" }
+        end,
+      },
+    },
+    formatters_by_ft = {
+      javascript = { "prettierd", "prettier" },
+      javascriptreact = { "prettierd", "prettier" },
+      typescript = { "prettierd", "prettier" },
+      typescriptreact = { "prettierd", "prettier" },
+      vue = { "prettierd", "prettier" },
+      css = { "prettierd", "prettier" },
+      scss = { "prettierd", "prettier" },
+      less = { "prettierd", "prettier" },
+      html = { "prettier" }, --NOTE: prettierd cause error with .ejs
+      jsonc = { "prettierd", "prettier" },
+      yaml = { "prettierd", "prettier" },
+      markdown = { "prettierd", "prettier" },
+      ["markdown.mdx"] = { "prettierd", "prettier" },
+      json = { "prettierd", "prettier" },
+      lua = { "stylua" },
+      go = { "gofumpt", "goimports-reviser" },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+    notify_on_error = true,
+  }
+end
 
 return {
   "stevearc/conform.nvim",
   event = "VeryLazy",
   cmd = { "ConformInfo" },
   keys = keys,
-  opts = opts,
+  config = config,
 }
