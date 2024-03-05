c8b587e42f687b931526c8f7abbad787cd53f331
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-03-06 00:28:13 +0700

    feat(nvim): add mini plugins

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,15 +1,53 @@
+-- List of mini plugins
 return {
-	{
-		"echasnovski/mini.bufremove",
-		opts = {},
-		keys = {
-			{
-				"<leader>bd",
-				function()
-					require("mini.bufremove").delete(0, false)
-				end,
-				desc = "Delete current buffer",
-			},
-		},
-	},
+  {
+    "echasnovski/mini.bufremove",
+    opts = {},
+    keys = {
+      {
+        "<leader>bd",
+        function()
+          require("mini.bufremove").delete(0, false)
+        end,
+        desc = "Delete current buffer",
+      },
+    },
+  },
+  --  - va)  - [V]isually select [A]round [)]paren
+  --  - yinq - [Y]ank [I]nside [N]ext [']quote
+  --  - ci'  - [C]hange [I]nside [']quote
+  {
+    "echasnovski/mini.ai",
+    event = "VeryLazy",
+    dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
+    config = function()
+      local miniai = require "mini.ai"
+
+      return {
+        n_lines = 300,
+        custom_textobjects = {
+          f = miniai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+        },
+        -- Disable error feedback.
+        silent = true,
+        -- Don't use the previous or next text object.
+        search_method = "cover",
+        mappings = {
+          -- Disable next/last variants.
+          around_next = "",
+          inside_next = "",
+          around_last = "",
+          inside_last = "",
+        },
+      }
+    end,
+  },
+  -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+  -- - sd'   - [S]urround [D]elete [']quotes
+  -- - sr)'  - [S]urround [R]eplace [)] [']
+  {
+    "echasnovski/mini.surround",
+    event = "VeryLazy",
+    opts = {},
+  },
 }
