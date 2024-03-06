d3691ed4d7295917e1ed04fb11159cb77ffdc6c3
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-03-07 01:01:19 +0700

    feat(wezterm): config and change fonts

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,28 +1,47 @@
-local wezterm = require("wezterm")
+local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
 config.term = "wezterm"
 config.front_end = "WebGpu"
+config.webgpu_power_preference = "HighPerformance"
 
 config.colors = {}
 config.colors.background = "#111111"
 config.colors.tab_bar = {
-	background = "#1f2329",
+  background = "#1f2329",
 }
 
 config.font_size = 12
-config.font = wezterm.font("FiraMono Nerd Font")
+config.font = wezterm.font { family = "FiraCode Nerd Font" }
+config.bold_brightens_ansi_colors = true
+config.font_rules = {
+  {
+    intensity = "Bold",
+    italic = true,
+    font = wezterm.font { family = "Maple Mono NF", weight = "Bold", style = "Italic" },
+  },
+  {
+    italic = true,
+    intensity = "Half",
+    font = wezterm.font { family = "Maple Mono NF", weight = "DemiBold", style = "Italic" },
+  },
+  {
+    italic = true,
+    intensity = "Normal",
+    font = wezterm.font { family = "Maple Mono NF", style = "Italic" },
+  },
+}
 
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 
 config.inactive_pane_hsb = {
-	saturation = 0.6,
-	brightness = 0.6,
+  saturation = 0.6,
+  brightness = 0.6,
 }
 
 config.hide_tab_bar_if_only_one_tab = true
