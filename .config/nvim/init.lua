34f7c50f9fed0c7a65b29134a751a95c354553bc
nhxnl <nahnera@gmail.com>
2024-03-07 15:05:39 +0700

    chore(nvim): move lazy config to init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,42 @@
-require("config")
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require "config.options"
+require "config.keymaps"
+require "config.autocmds"
+
+require("lazy").setup("plugins", {
+  defaults = { lazy = true },
+  ui = { border = "rounded" },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "netrwPlugin",
+        "rplugin",
+        "tarPlugin",
+        "tohtml",
+        "zipPlugin",
+        "tar",
+        "netrw",
+        "matchit",
+        --'matchparen',
+        "tutor",
+      },
+    },
+  },
+})
--- .config/nvim/lua/config/init.lua
+++
@@ -1,4 +0,0 @@
-require("config.lazy")
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
