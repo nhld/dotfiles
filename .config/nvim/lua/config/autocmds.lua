837fc3d17b6b001b8793c18df70f6431fae567a6
nhld <nahnera@gmail.com>
2024-03-11 00:19:12 +0700

    feat(nvim): config autocmds and icons

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,39 +1,58 @@
 local function augroup(name)
-	return vim.api.nvim_create_augroup("" .. name, { clear = true })
+  return vim.api.nvim_create_augroup("" .. name, { clear = true })
 end
 
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup "close_with_q",
+  pattern = {
+    "PlenaryTestPopup",
+    "help",
+    "lspinfo",
+    "qf",
+    "query",
+    "spectre_panel",
+    "startuptime",
+    "checkhealth",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
 vim.api.nvim_create_autocmd({ "TermOpen" }, {
-	callback = function()
-		vim.wo.number = false
-		vim.wo.relativenumber = false
-		vim.wo.spell = false
-		vim.cmd("startinsert")
-	end,
+  callback = function()
+    vim.wo.number = false
+    vim.wo.relativenumber = false
+    vim.wo.spell = false
+    vim.cmd "startinsert"
+  end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	group = augroup("wrap_spell"),
-	pattern = { "gitcommit", "markdown" },
-	callback = function()
-		vim.opt_local.wrap = true
-		vim.opt_local.spell = true
-	end,
+  group = augroup "wrap_spell",
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	group = augroup("json_conceal"),
-	pattern = { "json", "jsonc", "json5" },
-	callback = function()
-		vim.opt_local.conceallevel = 0
-	end,
+  group = augroup "json_conceal",
+  pattern = { "json", "jsonc", "json5" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
 })
 
 -- resize splits if window got resized
 vim.api.nvim_create_autocmd({ "VimResized" }, {
-	group = augroup("resize_splits"),
-	callback = function()
-		local current_tab = vim.fn.tabpagenr()
-		vim.cmd("tabdo wincmd =")
-		vim.cmd("tabnext " .. current_tab)
-	end,
+  group = augroup "resize_splits",
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd "tabdo wincmd ="
+    vim.cmd("tabnext " .. current_tab)
+  end,
 })
