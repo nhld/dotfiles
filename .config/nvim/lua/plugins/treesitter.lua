d343f360015b463cd5460a76f5a4075ea4d5f1cc
nhld <nahnera@gmail.com>
2024-03-11 00:22:23 +0700

    feat(nvim): add treesitter-context, remove fugitive and rhubarb plugin

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,97 +1,136 @@
-local config = vim.defer_fn(function()
-	require("nvim-treesitter.configs").setup({
-		ensure_installed = {
-			"cpp",
-			"go",
-			"lua",
-			"python",
-			"tsx",
-			"javascript",
-			"typescript",
-			"vimdoc",
-			"vim",
-			"html",
-			"json",
-			"css",
-			"markdown",
-			"markdown_inline",
-		},
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-		indent = { enable = true },
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<c-space>",
-				node_incremental = "<c-space>",
-				scope_incremental = "<c-s>",
-				node_decremental = "<M-space>",
-			},
-		},
-		textobjects = {
-			select = {
-				enable = true,
-				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-				keymaps = {
-					-- You can use the capture groups defined in textobjects.scm
-					["aa"] = "@parameter.outer",
-					["ia"] = "@parameter.inner",
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["ac"] = "@class.outer",
-					["ic"] = "@class.inner",
-				},
-			},
-			move = {
-				enable = true,
-				set_jumps = true, -- whether to set jumps in the jumplist
-				goto_next_start = {
-					["]m"] = "@function.outer",
-					["]]"] = "@class.outer",
-				},
-				goto_next_end = {
-					["]M"] = "@function.outer",
-					["]["] = "@class.outer",
-				},
-				goto_previous_start = {
-					["[m"] = "@function.outer",
-					["[["] = "@class.outer",
-				},
-				goto_previous_end = {
-					["[M"] = "@function.outer",
-					["[]"] = "@class.outer",
-				},
-			},
-			swap = {
-				enable = true,
-				swap_next = {
-					["<leader>a"] = "@parameter.inner",
-				},
-				swap_previous = {
-					["<leader>A"] = "@parameter.inner",
-				},
-			},
-		},
-	})
-end, 0)
+local config = function()
+  require("nvim-treesitter.configs").setup {
+    ensure_installed = {
+      "c",
+      "cpp",
+      "go",
+      "lua",
+      "python",
+      "tsx",
+      "javascript",
+      "typescript",
+      "vimdoc",
+      "vim",
+      "html",
+      "json",
+      "css",
+      "markdown",
+      "markdown_inline",
+      "fish",
+      "gitcommit",
+      "toml",
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+    indent = { enable = true },
+    autotag = {
+      enable = true,
+    },
+    folding = {
+      enable = true,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<c-space>",
+        node_incremental = "<c-space>",
+        scope_incremental = "<c-s>",
+        node_decremental = "<M-space>",
+      },
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ["aa"] = "@parameter.outer",
+          ["ia"] = "@parameter.inner",
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          ["]m"] = "@function.outer",
+          ["]]"] = "@class.outer",
+        },
+        goto_next_end = {
+          ["]M"] = "@function.outer",
+          ["]["] = "@class.outer",
+        },
+        goto_previous_start = {
+          ["[m"] = "@function.outer",
+          ["[["] = "@class.outer",
+        },
+        goto_previous_end = {
+          ["[M"] = "@function.outer",
+          ["[]"] = "@class.outer",
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<leader>a"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<leader>A"] = "@parameter.inner",
+        },
+      },
+    },
+  }
+end
 
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPost", "BufNewFile" },
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		{
-			"windwp/nvim-ts-autotag",
-			opts = {
-				enable_rename = true,
-				enable_close = true,
-				enable_close_on_splash = true,
-			},
-		},
-	},
-	build = ":TSUpdate",
-	config = config,
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPost", "BufNewFile" },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    {
+      "windwp/nvim-ts-autotag",
+      opts = {
+        enable_rename = true,
+        enable_close = true,
+        enable_close_on_splash = true,
+      },
+    },
+    {
+      "nvim-treesitter/nvim-treesitter-context",
+      opts = {
+        -- Avoid the sticky context from growing a lot.
+        max_lines = 3,
+        -- Match the context lines to the source code.
+        multiline_threshold = 1,
+        -- Disable it when the window is too small.
+        min_window_height = 20,
+      },
+      keys = {
+        {
+          "[c",
+          function()
+            -- Jump to previous change when in diffview.
+            if vim.wo.diff then
+              return "[c"
+            else
+              vim.schedule(function()
+                require("treesitter-context").go_to_context()
+              end)
+              return "<Ignore>"
+            end
+          end,
+          desc = "Jump to upper context",
+          expr = true,
+        },
+      },
+    },
+  },
+  build = ":TSUpdate",
+  config = config,
 }
