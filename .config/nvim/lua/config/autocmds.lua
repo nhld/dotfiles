8cc6778fe89fbccbcc89ebe2480c8748d65da0d8
nhld <nahnera@gmail.com>
2024-03-18 02:09:38 +0700

    feat(nvim): add auto commands

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -56,3 +56,24 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
     vim.cmd("tabnext " .. current_tab)
   end,
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = vim.api.nvim_create_augroup("n/last_location", { clear = true }),
+  desc = "Go to the last location when opening a buffer",
+  callback = function(args)
+    local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+    local line_count = vim.api.nvim_buf_line_count(args.buf)
+    if mark[1] > 0 and mark[1] <= line_count then
+      vim.cmd 'normal! g`"zz'
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("n/yank_highlight", { clear = true }),
+  desc = "Highlight on yank",
+  callback = function()
+    -- Setting a priority higher than the LSP references one.
+    vim.highlight.on_yank { higroup = "Visual", priority = 250 }
+  end,
+})
