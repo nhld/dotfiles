372340ad0259ce88be4830df4b502e29a74fafbc
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-05-20 09:21:07 +0700

    fix(nvim): remove keymaps which is now builtin

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -17,9 +17,6 @@ keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true }
 keymap.set("n", "gk", "k", { silent = true })
 keymap.set("n", "gj", "j", { silent = true })
 -- Diagnostic keymaps
-keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 -- Indent while remaining in visual mode
 keymap.set("v", "<", "<gv")
@@ -67,4 +64,3 @@ keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move down" })
 keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move up" })
 -- Plugins keymaps
 keymap.set("n", "<leader>L", "<cmd>Lazy<CR>", { desc = "Open Lazy plugin manager" })
-keymap.set("n", "<C-b>", "<cmd>Neotree toggle<CR>", { desc = "Toggle Neotree" })
