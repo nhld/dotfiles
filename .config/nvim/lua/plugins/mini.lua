0795618bd2afb75fc78dd957a73160ce754b5767
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-07-25 12:50:15 +0700

    feat(nvim): opts mini icons for ts files

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,4 +1,4 @@
--- List of mini plugins
+-- List of mini plugins: bufremove, ai, surround, hipatterns, icons
 return {
   {
     "echasnovski/mini.bufremove",
@@ -64,12 +64,24 @@ return {
   },
   {
     "echasnovski/mini.icons",
-    opts = {},
+    opts = {
+      file = {
+        [".eslintrc.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+        [".node-version"] = { glyph = "", hl = "MiniIconsGreen" },
+        [".prettierrc"] = { glyph = "", hl = "MiniIconsPurple" },
+        [".yarnrc.yml"] = { glyph = "", hl = "MiniIconsBlue" },
+        ["eslint.config.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+        ["package.json"] = { glyph = "", hl = "MiniIconsGreen" },
+        ["tsconfig.json"] = { glyph = "", hl = "MiniIconsAzure" },
+        ["tsconfig.build.json"] = { glyph = "", hl = "MiniIconsAzure" },
+        ["yarn.lock"] = { glyph = "", hl = "MiniIconsBlue" },
+      },
+    },
     event = "VeryLazy",
     specs = {
       { "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
     },
-    init = function()
+    init = function() ---@diagnostic disable-next-line
       package.preload["nvim-web-devicons"] = function()
         require("mini.icons").mock_nvim_web_devicons()
         return package.loaded["nvim-web-devicons"]
