44ffbfe0930581faaeecad2d2386926023515ef1
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-08-29 02:48:49 +0700

    feat(nvim): add treesitter disable function

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -27,6 +27,17 @@ local config = function()
     highlight = {
       enable = true,
       additional_vim_regex_highlighting = false,
+      disable = function(_, buf)
+        if not vim.bo[buf].modifiable then
+          return false
+        end
+
+        local max_filesize = 200 * 1024 -- 200 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
     },
     indent = { enable = true },
     folding = {
