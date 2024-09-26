b7cc4d9bd292fd29054faeac1fdc6612ffdd3810
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-09-26 20:51:22 +0700

    fix(nvim): tweak conform formatters
    - use dprint instead of prettier, prettierd
    - add toggle format on save keymap

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -9,29 +9,16 @@ local keys = {
   },
 }
 
-local fmt = { "prettier", "prettierd", stop_after_first = true }
-
 return {
   "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  event = { "BufWritePre", "LspAttach" },
   cmd = { "ConformInfo" },
   keys = keys,
   opts = {
     formatters_by_ft = {
-      javascript = fmt,
-      javascriptreact = fmt,
-      typescript = fmt,
-      typescriptreact = fmt,
-      vue = fmt,
-      css = fmt,
-      scss = fmt,
-      less = fmt,
-      html = { "prettier" },
-      jsonc = fmt,
-      yaml = fmt,
-      markdown = fmt,
-      ["markdown.mdx"] = fmt,
-      json = fmt,
+      css = { "stylelint" },
+      scss = { "stylelint" },
+      less = { "stylelint" },
       lua = { "stylua" },
       sh = { "shfmt" },
       bash = { "shfmt" },
@@ -40,15 +27,36 @@ return {
       go = { "gofumpt", "goimports-reviser", "goimports" },
     },
     default_format_opts = { lsp_format = "fallback" },
-    format_on_save = function()
-      if not vim.g.autoformat then
+    format_on_save = function(bufnr)
+      if not vim.g.format_on_save then
         return
       end
-      return { timeout_ms = 500 }
+      local res = { timeout_ms = 500 }
+      local filetype = vim.bo[bufnr].filetype
+      if
+        vim.tbl_contains({ "typescript", "javascript", "typescriptreact", "javascriptreact" }, filetype)
+        and require("conform").get_formatter_info("dprint", bufnr).available
+      then
+        res.lsp_format = "fallback"
+      else
+        res.lsp_format = vim.tbl_contains({ "c", "json", "jsonc", "rust" }, filetype) and "fallback" or nil
+      end
+      return res
     end,
     notify_on_error = false,
   },
   init = function()
-    vim.g.autoformat = true
+    vim.g.format_on_save = true
+    vim.keymap.set("n", "<leader>fm", ":lua ToggleFormatOnSave()<CR>", { noremap = true, silent = true, desc = "Toggle format on save" })
+
+    function ToggleFormatOnSave()
+      if vim.g.format_on_save then
+        vim.g.format_on_save = false
+        print "Format on save OFF"
+      else
+        vim.g.format_on_save = true
+        print "Format on save ON"
+      end
+    end
   end,
 }
