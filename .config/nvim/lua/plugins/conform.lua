38b0c82e4552954025a6bf02a914bc98554a42ef
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-10-03 02:46:56 +0700

    chore(nvim): cleanup conform `formatter_by_ft`

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -14,30 +14,14 @@ return {
   },
   opts = {
     formatters_by_ft = {
-      css = { "stylelint" },
-      scss = { "stylelint" },
-      less = { "stylelint" },
+      bash = { "shfmt" },
+      fish = { "fish_indent" },
       lua = { "stylua" },
       sh = { "shfmt" },
-      bash = { "shfmt" },
       zsh = { "shfmt" },
-      fish = { "fish_indent" },
-      go = { "gofumpt", "goimports-reviser", "goimports" },
     },
     default_format_opts = { lsp_format = "fallback" },
-    format_on_save = function(bufnr)
-      local res = { timeout_ms = 500 }
-      local filetype = vim.bo[bufnr].filetype
-      if
-        vim.tbl_contains({ "typescript", "javascript", "typescriptreact", "javascriptreact" }, filetype)
-        and require("conform").get_formatter_info("dprint", bufnr).available
-      then
-        res.lsp_format = "fallback"
-      else
-        res.lsp_format = vim.tbl_contains({ "c", "json", "jsonc", "rust" }, filetype) and "fallback" or nil
-      end
-      return res
-    end,
+    format_on_save = { timeout_ms = 500 },
     notify_on_error = false,
   },
 }
