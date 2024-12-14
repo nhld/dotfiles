b3731f22175c6c8166577ac6ffc1dc572ad7e00c
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-12-15 06:43:07 +0700

    feat(nvim): yank history is nice
    - add yanky again.

---
+++ .config/nvim/lua/plugins/yanky.lua
@@ -0,0 +1,20 @@
+return {
+  "gbprod/yanky.nvim",
+  keys = {
+    { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
+    { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
+    { "=p", "<Plug>(YankyPutAfterLinewise)", desc = "Put yanked text in line below" },
+    { "=P", "<Plug>(YankyPutBeforeLinewise)", desc = "Put yanked text in line above" },
+    { "[y", "<Plug>(YankyCycleForward)", desc = "Cycle forward through yank history" },
+    { "]y", "<Plug>(YankyCycleBackward)", desc = "Cycle backward through yank history" },
+    { "<leader>yy", ":Telescope yank_history<CR>", desc = "Yank history" },
+    { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yanky Yank" },
+  },
+  opts = {
+    ring = { history_length = 50 },
+    highlight = { timer = 250 },
+    preserve_cursor_position = {
+      enabled = true,
+    },
+  },
+}
