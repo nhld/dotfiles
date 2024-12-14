a073bda4e8c5ca75b4a81fdd77fa1ab894fb16f2
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-12-15 06:37:51 +0700

    fix(nvim): remove unused flash keymap

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -2,7 +2,6 @@
 local keys = {
   { "SS", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter", },
   { "ss", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash", },
-  { "r", mode = "o", function() require("flash").treesitter_search() end, desc = "Treesitter Search", },
 }
 
 return {
