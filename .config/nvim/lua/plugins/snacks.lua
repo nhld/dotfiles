d3fd8371d53069bb501085c08a4347b7cb083222
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-12-15 10:15:00 +0700

    fix(nvim): snacks global var

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -3,13 +3,17 @@ return {
   enabled = true,
   lazy = false,
   priority = 1000,
-  ---@diagnostic disable:undefined-global
   -- stylua: ignore
-  keys = {
+  keys = function()
+    local Snacks = require"snacks"
+    return {
     { "<leader>lg", function() Snacks.lazygit() end, desc = "Lazygit", },
     { "]]",         function() Snacks.words.jump(vim.v.count1, true) end, desc = "Next Reference", mode = { "n", "t" } },
     { "[[",         function() Snacks.words.jump(-vim.v.count1, true) end, desc = "Prev Reference", mode = { "n", "t" } },
-  },
+    { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+    { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
+  }
+  end,
   opts = {
     bigfile = { enabled = true },
     quickfile = { enabled = true },
