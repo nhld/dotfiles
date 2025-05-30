d9160ccc0e3998d2521bd9ceab3894654fbbc43f
Nhan Luu <62146587+nhld@users.noreply.github.com>
2025-05-30 14:23:16 +0700

    feat(nvim): add clangd lsp config

---
+++ .config/nvim/lsp/clangd.lua
@@ -0,0 +1,19 @@
+-- Install with
+-- mac: brew install llvm
+
+---@type vim.lsp.Config
+return {
+  cmd = {
+    "clangd",
+    "--clang-tidy",
+    "--header-insertion=iwyu",
+    "--completion-style=detailed",
+    "--fallback-style=none",
+    "--function-arg-placeholders=false",
+  },
+  filetypes = { "c", "cpp" },
+  root_markers = { ".clangd" },
+  init_options = {
+    fallbackFlags = { "-std=c++17" },
+  },
+}
