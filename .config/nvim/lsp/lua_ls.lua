bdf0b50cac05ad8b4aba3bc4f0faa482548898e7
Nhan Luu <62146587+nhld@users.noreply.github.com>
2025-05-30 14:21:40 +0700

    fix(nvim): move lsp server config

---
+++ .config/nvim/lsp/lua_ls.lua
@@ -0,0 +1,30 @@
+-- Install with
+-- brew install lua-language-server
+
+---@type vim.lsp.Config
+return {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = { ".luarc.json", ".luarc.jsonc" },
+  settings = {
+    Lua = {
+      completion = { callSnippet = "Replace" },
+      format = { enable = false },
+      hint = {
+        enable = true,
+        arrayIndex = "Disable",
+      },
+      runtime = {
+        version = "LuaJIT",
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          "${3rd}/luv/library",
+        },
+      },
+      telemetry = { enable = false },
+    },
+  },
+}
